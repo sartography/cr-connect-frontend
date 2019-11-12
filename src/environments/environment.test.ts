50ca2e948067d348e845fab1291fe96349332998
Aaron Louie <aaron@sartography.com>
2019-11-12 15:12:42 -0500

    Uses JSON for now

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  override_config_url: 'http://crconnect.sartography.com:5000/api/config'
+  // override_config_url: 'http://crconnect.sartography.com:5000/api/config',
+  override_config_url: '/assets/json/config.json'
 };
