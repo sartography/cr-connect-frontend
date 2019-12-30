042671e0768678e0fe9b7d4c689216630a3f685d
Aaron Louie <aaron@sartography.com>
2019-12-30 16:51:59 -0500

    Updates study attributes to match model changes.

--- src/app/study-card/study-card.component.ts
+++ src/app/study-card/study-card.component.ts
@@ -8,10 +8,18 @@ import {Study} from '../_models/study';
 })
 export class StudyCardComponent implements OnInit {
   @Input() study: Study;
+  percentComplete: number;
 
-  constructor() { }
+  constructor() {
+    this.percentComplete = this.calculatePercentComplete();
+  }
 
   ngOnInit() {
   }
 
+  // TODO: Calculate percent complete based on number of tasks in workflow.
+  calculatePercentComplete() {
+    // For now, just return a random number.
+    return Math.floor(Math.random() * 100);
+  }
 }
