0c39c7535a794f7dddaa39f6210a80df44cda2b5
Dan Funk <daniel.h.funk@gmail.com>
2019-12-31 09:55:27 -0500

    Using http for now, till we get the system properly configured (I'd like to wait and let Fred set this up when we are rolling)

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ import {AppEnvironment} from '../app/_models/app-environment';
 
 export const environment: AppEnvironment = {
   production: true,
-  api: 'https://workflow.sartography.com:5000',
+  api: 'http://workflow.sartography.com:5000',
   googleAnalyticsKey: '',
   irbUrl: 'https://www.irb.virginia.edu/index.cfm?fuseAction=ss_create.create',
 };
