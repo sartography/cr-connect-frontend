19f1ec3e243a3e1459e0a187037cffe433621cc3
Aaron Louie <ajlouie@gmail.com>
2020-02-03 16:25:39 -0500

    Fixes failing test

--- src/app/file-upload/file-upload.component.spec.ts
+++ src/app/file-upload/file-upload.component.spec.ts
@@ -1,45 +1,36 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {NgProgressModule} from '@ngx-progressbar/core';
+import {NgxFileDropModule} from 'ngx-file-drop';
 import { FileUploadComponent } from './file-upload.component';
 import { MatTableModule } from '@angular/material';
-import { ResourceApiService } from '../shared/resource-api/resource-api.service';
-import { MockResourceApiService } from '../shared/mocks/resource-api.service.mock';
 import { ReactiveFormsModule, FormsModule, FormControl } from '@angular/forms';
-import { FormField } from '../form-field';
-import { FileAttachment } from '../file-attachment';
 
 describe('FileUploadComponent', () => {
   let component: FileUploadComponent;
   let fixture: ComponentFixture<FileUploadComponent>;
-  let api: MockResourceApiService;
 
   beforeEach(async(() => {
-    api = new MockResourceApiService();
 
     TestBed
       .configureTestingModule({
         declarations: [FileUploadComponent],
         imports: [
           FormsModule,
+          MatFormFieldModule,
+          MatIconModule,
           MatTableModule,
-          ReactiveFormsModule
+          NgProgressModule,
+          NgxFileDropModule,
+          ReactiveFormsModule,
         ],
-        providers: [
-          { provide: ResourceApiService, useValue: api }
-        ],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA]
       })
       .compileComponents()
       .then(() => {
         fixture = TestBed.createComponent(FileUploadComponent);
         component = fixture.componentInstance;
-        component.field = new FormField({
-          formControl: new FormControl(),
-          attachments: new Map<number | string, FileAttachment>(),
-          required: false,
-          placeholder: 'Attachments',
-          type: 'files'
-        });
         fixture.detectChanges();
       });
   }));
