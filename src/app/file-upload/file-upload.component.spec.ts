0dba51d1873cdbbe8dc01ea6f4fb7e252ab9c108
Aaron Louie <ajlouie@gmail.com>
2020-02-03 22:22:41 -0500

    Updates sartography-workflow-lib. Updates API calls to match new endpoint params structure.

--- src/app/file-upload/file-upload.component.spec.ts
+++ src/app/file-upload/file-upload.component.spec.ts
@@ -1,14 +1,15 @@
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatTableModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import {ActivatedRoute, convertToParamMap} from '@angular/router';
 import {NgProgressModule} from '@ngx-progressbar/core';
 import {NgxFileDropModule} from 'ngx-file-drop';
-import {ApiService, MockEnvironment} from 'sartography-workflow-lib';
-import { FileUploadComponent } from './file-upload.component';
-import { MatTableModule } from '@angular/material';
-import { ReactiveFormsModule, FormsModule, FormControl } from '@angular/forms';
+import {of} from 'rxjs';
+import {ApiService, MockEnvironment, mockFileMetas, mockWorkflow0} from 'sartography-workflow-lib';
+import {FileUploadComponent} from './file-upload.component';
 
 describe('FileUploadComponent', () => {
   let component: FileUploadComponent;
@@ -32,6 +33,10 @@ describe('FileUploadComponent', () => {
         ],
         providers: [
           ApiService,
+          {
+            provide: ActivatedRoute,
+            useValue: {paramMap: of(convertToParamMap({study_id: '0', workflow_id: '0', task_id: '0'}))},
+          },
           {provide: 'APP_ENVIRONMENT', useClass: MockEnvironment},
         ]
       })
@@ -41,6 +46,26 @@ describe('FileUploadComponent', () => {
         fixture = TestBed.createComponent(FileUploadComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+
+        const wfReq = httpMock.expectOne('apiRoot/workflow/0');
+        expect(wfReq.request.method).toEqual('GET');
+        wfReq.flush(mockWorkflow0);
+        expect((component as any).workflowId).toEqual(mockWorkflow0.id);
+
+        const fmsReq = httpMock.expectOne('apiRoot/file?task_id=0');
+        expect(fmsReq.request.method).toEqual('GET');
+        fmsReq.flush(mockFileMetas);
+        expect((component as any).fileMetas).toEqual(mockFileMetas);
+
+        const fileArray = [];
+        mockFileMetas.forEach((fm, i) => {
+          const fReq = httpMock.expectOne(`apiRoot/file/${fm.id}/data`);
+          expect(fReq.request.method).toEqual('GET');
+          fReq.flush(mockFileMetas[i].file);
+          fileArray.push(mockFileMetas[i].file);
+        });
+
+        expect(Array.from((component as any).files as Set<File>)).toEqual(fileArray);
       });
   }));
 
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input, ViewChild} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {FormlyTemplateOptions} from '@ngx-formly/core';
 import {FieldType} from '@ngx-formly/material';
 import {NgProgressComponent} from '@ngx-progressbar/core';
 import {FileSystemFileEntry, NgxFileDropEntry} from 'ngx-file-drop';
 import {ReplaySubject} from 'rxjs';
-import {ApiService} from 'sartography-workflow-lib';
+import {ApiService, FileMeta, FileType, Workflow} from 'sartography-workflow-lib';
 
 @Component({
   selector: 'app-file-upload',
@@ -15,6 +16,7 @@ export class FileUploadComponent extends FieldType {
   @Input() to: FormlyTemplateOptions;
   droppedFiles: NgxFileDropEntry[] = [];
   files = new Set<File>();
+  fileMetas: FileMeta[] = [];
   updateFilesSubject = new ReplaySubject<File[]>();
   displayedColumns: string[] = [
     'name',
@@ -26,9 +28,25 @@ export class FileUploadComponent extends FieldType {
   ];
   dropZoneHover = false;
   @ViewChild(NgProgressComponent, {static: false}) progress: NgProgressComponent;
+  private workflowSpecId: string;
+  private studyId: number;
+  private workflowId: number;
+  private taskId: string;
 
-  constructor(private api: ApiService) {
+  constructor(
+    private api: ApiService,
+    private activatedRoute: ActivatedRoute,
+  ) {
     super();
+    activatedRoute.paramMap.subscribe(paramMap => {
+      this.studyId = parseInt(paramMap.get('study_id'), 10);
+      this.workflowId = parseInt(paramMap.get('workflow_id'), 10);
+      this.taskId = paramMap.get('task_id');
+      this.api.getWorkflow(this.workflowId).subscribe(wf => {
+        this.workflowSpecId = wf.workflow_spec_id;
+        this.loadFiles();
+      });
+    });
   }
 
   dropped(droppedFiles: NgxFileDropEntry[]) {
@@ -87,14 +105,18 @@ export class FileUploadComponent extends FieldType {
     }
   }
 
-  fileIcon(file: File): string {
+  getFileType(file: File): FileType {
     const s = file.type || file.name;
     const nameArray = s.toLowerCase().split(file.type ? '/' : '.');
     if (nameArray.length > 0) {
-      return `/assets/icons/file_types/${nameArray[nameArray.length - 1]}.svg`;
+      return FileType[nameArray[nameArray.length - 1]];
     } else {
-      return `/assets/icons/file_types/unknown.svg`;
+      return FileType.UNKNOWN;
     }
+  }
+
+  fileIcon(file: File): string {
+    return `/assets/icons/file_types/${this.getFileType(file)}.svg`;
   }
 
   addFile(file: File) {
@@ -102,6 +124,15 @@ export class FileUploadComponent extends FieldType {
 
     //  TODO: Add file
     this.files.add(file);
+    const fileMeta: FileMeta = {
+      content_type: file.type,
+      name: file.name,
+      type: this.getFileType(file),
+      file,
+      task_id: this.taskId,
+      study_id: this.studyId,
+    };
+    this.api.addFileMeta({study_id: this.studyId, task_id: this.taskId}, fileMeta);
     this.updateFileList();
   }
 
@@ -130,5 +161,14 @@ export class FileUploadComponent extends FieldType {
   updateDisplayName($event, file: File) {
     console.log('=== updateDisplayName ===');
     this.editFileMeta(file, {display_name: $event.target.value});
+  }
+
+  private loadFiles() {
+    this.api.getFileMeta({task_id: this.taskId}).subscribe(fms => {
+      this.fileMetas = fms;
+      this.fileMetas.forEach(fm => {
+        this.api.getFileData(fm.id).subscribe(file => this.files.add(file as File));
+      });
+    });
   }
 }
