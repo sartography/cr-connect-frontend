5e72d7c0bba8a44305b4641c21dcef983609b38a
Aaron Louie <ajlouie@gmail.com>
2020-02-11 20:19:48 -0500

    Re-initializes form on initialization or task change.

--- src/app/workflow-form/workflow-form.component.ts
+++ src/app/workflow-form/workflow-form.component.ts
@@ -40,6 +40,7 @@ export class WorkflowFormComponent implements OnInit, OnChanges {
   }
 
   private _loadModel(task: WorkflowTask) {
+    this.form = new FormGroup({});
     if (task && task.data) {
       this.model = createClone()(task.data);
       const label = `Data for Workflow Task: '${task.name} (${task.id})'`;
