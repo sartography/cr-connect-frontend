291614f29153490eefacce0c87c3578e52f863d6
Aaron Louie <ajlouie@gmail.com>
2020-02-14 11:25:37 -0500

    Injects form builder, field, and control

--- src/app/_forms/file-upload/file-upload.component.spec.ts
+++ src/app/_forms/file-upload/file-upload.component.spec.ts
@@ -1,23 +1,31 @@
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {AbstractControl, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {async, ComponentFixture, inject, TestBed} from '@angular/core/testing';
+import {AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatTableModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {ActivatedRoute, convertToParamMap} from '@angular/router';
+import {FormlyFormBuilder, FormlyModule} from '@ngx-formly/core';
+import {FormlyFieldConfigCache} from '@ngx-formly/core/lib/components/formly.field.config';
 import {FileSystemFileEntry, NgxFileDropEntry, NgxFileDropModule} from 'ngx-file-drop';
 import {of} from 'rxjs';
 import {ApiService, MockEnvironment, mockFileMeta0, mockFileMetas} from 'sartography-workflow-lib';
+import {FormlyConfig} from '../../app.module';
 import {FileUploadComponent} from './file-upload.component';
 
 describe('FileUploadComponent', () => {
   let component: FileUploadComponent;
   let fixture: ComponentFixture<FileUploadComponent>;
   let httpMock: HttpTestingController;
+  let builder: FormlyFormBuilder;
+  let form: FormGroup;
+  let field: FormlyFieldConfigCache;
+  let config: FormlyConfig;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        FormlyModule.forRoot(config),
         FormsModule,
         HttpClientTestingModule,
         MatFormFieldModule,
@@ -36,16 +44,28 @@ describe('FileUploadComponent', () => {
           useValue: {paramMap: of(convertToParamMap({study_id: '0', workflow_id: '0', task_id: '0'}))},
         },
         {provide: 'APP_ENVIRONMENT', useClass: MockEnvironment},
+        FormlyConfig,
       ]
     })
       .compileComponents();
+  }));
+
+  beforeEach(inject([FormlyFormBuilder, FormlyConfig], (formlyBuilder: FormlyFormBuilder, formlyConfig: FormlyConfig) => {
+    form = new FormGroup({});
+    config = formlyConfig;
+    builder = formlyBuilder;
+    field = {
+      key: 'hi',
+      defaultValue: 'Hello there.'
+    };
+    builder.buildForm(form, [field], {}, {});
   }));
 
   beforeEach(() => {
     httpMock = TestBed.get(HttpTestingController);
     fixture = TestBed.createComponent(FileUploadComponent);
     component = fixture.componentInstance;
-    component.field = {key: 'hi'};
+    component.field = field;
     fixture.detectChanges();
 
     const fmsReq = httpMock.expectOne('apiRoot/file?study_id=0&workflow_id=0&task_id=0&form_field_key=hi');
