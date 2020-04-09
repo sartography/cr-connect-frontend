51398ad6de21dc18c3d571d0f386a261a5ff9faf
Aaron Louie <ajlouie@gmail.com>
2020-04-09 14:58:06 -0400

    Sorts workflow by display order

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {
-  Study,
-  WorkflowSpecCategory,
-} from 'sartography-workflow-lib';
+import {Study, WorkflowSpecCategory, WorkflowState, WorkflowStatus,} from 'sartography-workflow-lib';
 
 
 @Component({
@@ -13,12 +10,21 @@ import {
 export class DashboardComponent implements OnInit {
   @Input() study: Study;
   categoryTabs: WorkflowSpecCategory[];
+  statuses = WorkflowStatus;
+  states = WorkflowState;
 
   constructor() {
   }
 
   ngOnInit() {
     this.categoryTabs = this.study.categories
-      .sort((a, b) => (a.display_order < b.display_order) ? -1 : 1);
+      .sort((a, b) => (a.display_order < b.display_order) ? -1 : 1)
+      .map(cat => {
+        cat.workflows = cat.workflows
+          .filter(wf => wf.state !== WorkflowState.HIDDEN)
+          .sort((a, b) => (a.display_order < b.display_order) ? -1 : 1);
+        return cat;
+      })
+      .filter(cat => cat.workflows.length > 0);
   }
 }
