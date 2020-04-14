3892323ee01948c0ed3f291196a3a0705e987430
Aaron Louie <ajlouie@gmail.com>
2020-04-14 15:41:21 -0400

    Runs headless

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,11 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+
+  if (config.browsers.indexOf('ChromeHeadlessCI') !== -1) {
+    process.env.CHROME_BIN = require('puppeteer').executablePath();
+  }
+
   config.set({
     files: ['karma.globals.js'],
     basePath: '',
@@ -20,6 +25,12 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage'),
       reports: ['lcovonly'],
       fixWebpackSourcePaths: true
+    },
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu' ]
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
