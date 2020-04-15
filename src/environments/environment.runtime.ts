df9139af9d8027a381b83074e8d89229e493e067
Aaron Louie <ajlouie@gmail.com>
2020-04-15 09:47:19 -0400

    Removes curly braces from envsubst template strings

--- src/environments/environment.runtime.ts
+++ src/environments/environment.runtime.ts
@@ -3,7 +3,7 @@ import {AppEnvironment} from 'sartography-workflow-lib';
 declare var ENV;
 
 export const environment: AppEnvironment = {
-  production: ENV && ENV.production === '{$PRODUCTION}' ? false : (ENV.production === 'true'),
-  api: ENV && ENV.api === '{$API_URL}' ? 'http://localhost:5000/v1.0' : ENV.api,
-  irbUrl: ENV && ENV.irbUrl === '{$IRB_URL}' ? 'http://localhost:5001' : ENV.irbUrl,
+  production: ENV && ENV.production === '$PRODUCTION' ? false : (ENV.production === 'true'),
+  api: ENV && ENV.api === '$API_URL' ? 'http://localhost:5000/v1.0' : ENV.api,
+  irbUrl: ENV && ENV.irbUrl === '$IRB_URL' ? 'http://localhost:5001' : ENV.irbUrl,
 };
