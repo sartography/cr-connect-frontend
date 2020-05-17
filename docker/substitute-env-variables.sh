2803e8ad68224dca9674a29d664b26dfa9445f6e
Aaron Louie <ajlouie@gmail.com>
2020-05-16 23:38:26 -0400

    Prints some stuff out for troubleshooting

--- docker/substitute-env-variables.sh
+++ docker/substitute-env-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo 'Substituting environment variables...'
+
 # The first parameter is a comma-delimited list of paths to files which should be substituted
 if [[ -z $1 ]]; then
   echo 'ERROR: No target files given.'
@@ -9,14 +11,20 @@ fi
 env_list='\$PRODUCTION \$API_URL \$IRB_URL \$HOME_ROUTE \$PORT0'
 for i in $(echo $1 | sed "s/,/ /g")
 do
+  echo "replacing $env_list in $i"
+
   # Replace strings in the given file(s) in env_list
   envsubst "$env_list" < "$i" > "$i".tmp && mv "$i".tmp "$i"
+
+  echo "..."
 
   # Set DEBUG=true in order to log the replaced file
   if [ "$DEBUG" = true ] ; then
     exec cat $i
   fi
 done
+
+echo 'Finished substituting environment variables.'
 
 # Execute all other commands with parameters
 exec "${@:2}"
