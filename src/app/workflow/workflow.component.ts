8cbcbfa4c63f8cce6c7f5fb94236477d878eda68
Dan <daniel.h.funk@gmail.com>
2021-04-09 10:59:50 -0400

    Fixing a linting error

--- src/app/workflow/workflow.component.ts
+++ src/app/workflow/workflow.component.ts
@@ -77,7 +77,6 @@ export class WorkflowComponent implements OnInit {
         () => this.updateTaskList(this.workflow)
       );
     });
-    
     this.userService.isAdmin$.subscribe(a => {this.isAdmin = a;
       this.showDataPane = (!this.environment.hideDataPane) || (this.isAdmin);})
     this.userPreferencesService.preferences$.subscribe(p => {
