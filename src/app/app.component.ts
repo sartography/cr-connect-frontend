1507a3b1ce463f659512e152c6c7bd662fa69595
alicia pritchett <aliciateaford@gmail.com>
2022-01-10 12:39:59 -0500

    WIP - disable browser back button
    this is the only thing so far that has halfway worked but it's NO GOOD anyways

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {DomSanitizer, Title} from '@angular/platform-browser';
 import {NavigationEnd, Router} from '@angular/router';
 import {filter} from 'rxjs/operators';
 import {AppEnvironment, FileType, GoogleAnalyticsService} from 'sartography-workflow-lib';
+import {LocationStrategy} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -20,10 +21,12 @@ export class AppComponent {
     private domSanitizer: DomSanitizer,
     private googleAnalyticsService: GoogleAnalyticsService,
     private router: Router,
+    private location: LocationStrategy,
   ) {
     // const navEnd = this.router.events.pipe(filter(e => e instanceof NavigationEnd));
     // navEnd.subscribe(_ => this.googleAnalyticsService.init(this.environment.googleAnalyticsKey));
 
+    this.disableBack();
     const fileTypes = Object.values(FileType);
     fileTypes.forEach(t => {
       const url = this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/file_types/${t}.svg`)
@@ -35,5 +38,13 @@ export class AppComponent {
   reload() {
     this.loading = true;
     setTimeout(() => this.loading = false, 300);
+  }
+
+  disableBack() {
+    history.pushState(null, null, window.location.href);
+    this.location.onPopState(() => {
+      alert('Please use the in-application navigation.');
+      history.pushState(null, null, window.location.href);
+    })
   }
 }
