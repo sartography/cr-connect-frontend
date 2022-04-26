145928688578112e13386e977b3197d2d9334a45
Dan <daniel.h.funk@gmail.com>
2022-04-26 16:18:46 -0400

    Assure sentry errors are properly percolated up and reported.

--- src/environments/environment.runtime.ts
+++ src/environments/environment.runtime.ts
@@ -6,11 +6,12 @@ export const _has = (env, key, temp): boolean => env && ![null, undefined, temp,
 
 export const environment: AppEnvironment = {
   homeRoute: _has(ENV, 'homeRoute', '$HOME_ROUTE') ? ENV.homeRoute : 'home',
-  production: _has(ENV, 'production', '$PRODUCTION') ? (ENV.production === 'true') : false,
+  production: _has(ENV, 'production', '$PRODUCTION') ? (ENV.production === 'true') : true,
   hideDataPane: _has(ENV, 'hideDataPane', '$HIDE_DATA_PANE') ? (ENV.hideDataPane === 'true') : true,
   api: _has(ENV, 'api', '$API_URL') ? ENV.api : 'http://localhost:5000/v1.0',
   irbUrl: _has(ENV, 'irbUrl', '$IRB_URL') ? ENV.irbUrl : 'http://localhost:5001',
   title: _has(ENV, 'title', '$TITLE') ? ENV.title : 'Research Ramp-Up Toolkit',
   googleAnalyticsKey: _has(ENV, 'googleAnalyticsKey', '$GOOGLE_ANALYTICS_KEY') ? ENV.googleAnalyticsKey : 'UA-168203235-5',
-  sentryKey: _has(ENV, 'sentryKey', '$SENTRY_KEY') ? ENV.sentryKey : undefined,
+  sentryKey: _has(ENV, 'sentryKey', '$SENTRY_KEY') ? ENV.sentryKey : "",
+  sentryEnvironment: _has(ENV, 'sentryEnvironment', '$SENTRY_ENVIRONMENT') ? ENV.sentryEnvironment : "local",
 };
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
-import {AppModule} from './app/app.module';
+import * as Sentry from "@sentry/angular";
+import { BrowserTracing } from "@sentry/tracing";
+import { AppModule } from "./app/app.module";
 import {environment} from './environments/environment.runtime';
 
 if (environment.production) {
   enableProdMode();
 }
+Sentry.init({
+  dsn: environment.sentryKey,
+  environment: environment.sentryEnvironment,
+  integrations: [
+    // Registers and configures the Tracing integration,
+    // which automatically instruments your application to monitor its
+    // performance, including custom Angular routing instrumentation
+    new BrowserTracing({
+      tracingOrigins: ["localhost", "https://testing.crconnect.uvadcos.io", "https://staging.crconnect.uvadcos.io"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
 
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
